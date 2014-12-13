2a6fe95848be7c0937d65d9b30866aed74bf0473
Jean-Francois Laloux <jeanfrancois@laloux.me>
2014-12-13 12:42:28 -0200

    Clone

---
+++ js/app.js
@@ -0,0 +1,17 @@
+//window.onload = function () {
+  //var platform = window.navigator.platform;
+
+  //var os;
+
+  //if (platform.search(/mac/i) > -1) os = 'osx';
+  //if (platform.search(/win/i) > -1) os = 'windows';
+  //if (platform.search(/linux/i) > -1) os = 'linux';
+
+  //if (os) {
+    //var buttons = document.getElementsByClassName('dl-' + os);
+    //for (var i = 0; i < buttons.length; i++) {
+      //var button = buttons[i];
+      //button.className = button.className + ' button-primary';
+    //}
+  //}
+//}
